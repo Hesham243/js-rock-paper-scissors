364eb50db0e0ee26f4021558ef4a50c8c7483e1a
Hesham243 <heshamahmed243@gmail.com>
2025-06-29 10:45:21 +0300

    Cahch elements and added variables

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*------------------Constants---------------- */
+const choices = ['rock', 'paper', 'scissors']
+
+/*-------------------Variabls---------------- */
+let msg
+let playerChoice
+let computerChoise
+
+/*-------------------Cached Element Reference---------------- */
+const rockBtnEl = document.querySelector('#rock')
+
+const paperbtnEl = document.querySelector('#paper')
+
+const scissorsbtnEl = document.querySelector('#scissors')
+
+
+console.log(paperbtnEl)
+
+
+/*-------------------Functions---------------- */
